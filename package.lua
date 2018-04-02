a5a25d1112fb5b87b5dec2abf3ef1c4cbb8a6407
Ruzsa Balazs <cellux@omkamra.hu>
2018-04-02 20:12:15 +0200

    initial commit

---
+++ package.lua
@@ -0,0 +1,12 @@
+return {
+   package = "github.com/cellux/zz_audio",
+   imports = {
+      "github.com/cellux/zz_sdl2"
+   },
+   exports = { 
+      "audio"
+   },
+   depends = {
+      audio = { "trigger", "sdl2" }
+   }
+}
